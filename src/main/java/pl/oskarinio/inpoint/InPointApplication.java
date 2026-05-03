d6552021fed03cd812582ae16aa95c8b87c48a79
oSkarinio143 <oskarz143@onet.pl>
2026-05-03 17:03:08 +0200

    created first files, src, pom.xml

---
+++ src/main/java/pl/oskarinio/inpoint/InPointApplication.java
@@ -0,0 +1,13 @@
+package pl.oskarinio.inpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/oskarinio/inpoint/InPointApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oskarinio.inpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
